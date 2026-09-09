af99353a248e8d26255bd08a183b0901cfe1a45d
Fabricio-Lubczyk <fabriciosamuel737@gmail.com>
2026-09-09 16:35:46 -0300

    Valida dados de eventos

---
+++ app/Http/Requests/EventRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class EventRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'category_id' => ['required', 'integer', 'exists:categories,id'],
+            'venue_id' => ['required', 'integer', 'exists:venues,id'],
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string', 'max:5000'],
+            'starts_at' => ['required', 'date'],
+            'ends_at' => ['nullable', 'date', 'after:starts_at'],
+            'capacity' => ['required', 'integer', 'min:1'],
+            'status' => ['required', Rule::in(['rascunho', 'publicado', 'cancelado'])],
+        ];
+    }
+}
