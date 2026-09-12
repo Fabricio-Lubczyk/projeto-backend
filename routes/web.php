0cdb04041686f1583c608e51b69e3878fc272239
gross <engs-joaomatos@camporeal.edu.br>
2026-09-12 11:43:11 -0300

    adicionando as rotas de categorias e eventos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaEventoController;
+use App\Http\Controllers\EventoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('categorias-eventos', CategoriaEventoController::class)
+    ->parameters([
+        'categorias-eventos' => 'categoriaEvento',
+    ]);
+
+Route::resource('eventos', EventoController::class);
