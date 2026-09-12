af728fc133909ee92bf37e3233761589c665eefd
gross <engs-joaomatos@camporeal.edu.br>
2026-09-12 11:32:19 -0300

    Relacionando ususario com eventos organizados

--- database/migrations/2026_09_12_142156_criar_tabela_eventos.php
+++ database/migrations/2026_09_12_142156_criar_tabela_eventos.php
@@ -6,19 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('categoria_evento_id')
+                ->constrained('categorias_eventos');
+
+            $table->foreignId('usuario_id')
+                ->constrained('users');
+
+            $table->string('titulo');
+            $table->text('descricao');
+            $table->string('local');
+            $table->date('data_evento');
+            $table->time('horario_inicio');
+            $table->time('horario_fim');
+            $table->unsignedInteger('max_participantes');
+            $table->string('status')->default('ativo');
+
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('eventos');
     }
 };
