5336e4fd13e83a202161e4e308c8653bbe0f7b7c
gross <engs-joaomatos@camporeal.edu.br>
2026-09-12 11:23:51 -0300

    criando a tabela de eventos

--- database/migrations/2026_09_12_141250_criar_tabela_categorias_eventos.php
+++ database/migrations/2026_09_12_141250_criar_tabela_categorias_eventos.php
@@ -6,19 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::create('categorias_eventos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->text('descricao')->nullable();
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('categorias_eventos');
     }
 };
