b94594c6ca284e5802e301fb4d4cb59a0804c625
KawanSz3 <kawansouza348@gmail.com>
2026-09-13 16:23:42 -0300

    Relaciona eventos às inscrições

--- app/Models/Evento.php
+++ app/Models/Evento.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Evento extends Model
 {
@@ -44,5 +45,10 @@ class Evento extends Model
             User::class,
             'usuario_id'
         );
+    }
+
+    public function inscricoes(): HasMany
+    {
+        return $this->hasMany(Inscricao::class, 'evento_id');
     }
 }
