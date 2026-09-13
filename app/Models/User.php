2040e415f402856074ac2555bef7f9c8a1205e01
KawanSz3 <kawansouza348@gmail.com>
2026-09-13 16:22:29 -0300

    Corrige model de usuário

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
@@ -33,8 +32,9 @@ class User extends Authenticatable
             'role' => UserRole::class,
         ];
     }
+
     public function eventosOrganizados(): HasMany
     {
-    return $this->hasMany(Evento::class, 'usuario_id');
+        return $this->hasMany(Evento::class, 'usuario_id');
     }
 }
