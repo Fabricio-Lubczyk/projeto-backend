c2f7f5ef8e635355fb34762f9aa9fd51ceaaf689
KawanSz3 <kawansouza348@gmail.com>
2026-09-13 16:30:02 -0300

    Exibe erros nos formulários

--- resources/views/estruturas/principal.blade.php
+++ resources/views/estruturas/principal.blade.php
@@ -146,6 +146,16 @@
         </div>
     @endif
 
+    @if($errors->any())
+        <div class="erro">
+            <ul>
+                @foreach($errors->all() as $erro)
+                    <li>{{ $erro }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     @yield('conteudo')
 
 </div>
