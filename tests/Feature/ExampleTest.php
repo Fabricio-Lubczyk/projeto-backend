5b40885c217cf506bb869e0ad3318ffbc70dc9c4
Fabricio-Lubczyk <fabriciosamuel737@gmail.com>
2026-09-13 16:59:30 -0300

    Atualiza teste da página inicial

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,10 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_pagina_inicial_redireciona_para_eventos(): void
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertRedirect(route('eventos.index'));
     }
 }
