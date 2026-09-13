94fec119514ebca233721ed5f509db97ebd1e157
KawanSz3 <kawansouza348@gmail.com>
2026-09-13 16:26:08 -0300

    Adiciona policy de inscrições

--- app/Models/Inscricao.php
+++ app/Models/Inscricao.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Policies\InscricaoPolicy;
+use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+#[UsePolicy(InscricaoPolicy::class)]
 class Inscricao extends Model
 {
     protected $table = 'inscricoes';
